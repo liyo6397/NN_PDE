adce10baa9426cd8780b1fb4ab06f406f7b8079c
Yuan-Hsiang Lee <leeyh.tw@gmail.com>
2019-03-17 16:44:38 -0600

    actual indent updates

--- tests_dl_pde.py
+++ tests_dl_pde.py
@@ -1,9 +1,10 @@
 import unittest
 import numpy as np
 
+
 class TestDNNPDE(unittest.TestCase):
     def test_compute_dx(self):
         # given
-        x = np.linspace(0,1,10)
+        x = np.linspace(0, 1, 10)
 
         pass
